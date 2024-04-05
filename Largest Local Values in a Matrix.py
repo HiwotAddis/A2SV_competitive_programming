b33995aec4a2e58a13d86a977465dd214c76e8bf
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2024-04-06 01:58:11 +0300

    Create Largest Local Values in a Matrix.py

---
+++ Largest Local Values in a Matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        
+        n = len(grid)
+        maxLocal = [[0] * (n-2) for _ in range(n-2)]
+
+        for i in range(n-2):
+            for j in range(n-2):
+                submatrix = [
+                    [grid[i][j], grid[i][j+1], grid[i][j+2]],
+                    [grid[i+1][j], grid[i+1][j+1], grid[i+1][j+2]],
+                    [grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]
+                ]
+                maxLocal[i][j] = max(max(row) for row in submatrix)
+
+        return maxLocal
