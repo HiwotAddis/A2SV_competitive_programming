1cf04c6a5d2eac6606634ff760ae922ed131a16d
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2024-04-17 00:21:04 +0300

    Create Largest Merge Of Two Strings.py

---
+++ Largest Merge Of Two Strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        res=''
+        while word1 and word2:
+            if word1>word2:
+                res+=word1[0]
+                word1=word1[1:]
+            else:
+                res+=word2[0]
+                word2=word2[1:]
+        if word1:
+            res+=word1
+        if word2:
+            res+=word2
+        return res
+
