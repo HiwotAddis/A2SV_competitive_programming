5d7158b981996adff8e190d668b4920130c4a512
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2024-07-07 23:41:45 +0300

    Create Break a Palindrome.py

---
+++ Break a Palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        n = len(palindrome)
+        if n == 1:
+            return ""
+        palindrome_list = list(palindrome)
+        for i in range(n // 2):
+            if palindrome_list[i] != 'a':
+                palindrome_list[i] = 'a'
+                return "".join(palindrome_list)
+
+        palindrome_list[-1] = 'b'
+        return "".join(palindrome_list)
