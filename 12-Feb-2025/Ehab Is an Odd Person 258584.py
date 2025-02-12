2417d488cce22d705fd3f018e4f7f66862983879
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2025-02-12 05:19:13 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 12-Feb-2025/Ehab Is an Odd Person 258584.py
@@ -0,0 +1,12 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())  
+arr = list(map(int, input().split()))  
+has_even = any(x % 2 == 0 for x in arr)  
+has_odd = any(x % 2 == 1 for x in arr)  
+
+if has_even and has_odd:  
+    arr.sort()
+
+print(*arr)  
+
