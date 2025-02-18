30ba4e501971a46b2c8a3469cc0ba1dd626cafd1
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2025-02-18 19:44:25 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 18-Feb-2025/Karen and Coffee 272265.py
@@ -0,0 +1,24 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,q=map(int,input().split())
+prefix=[0]*(200000+2)
+for _ in range(n):
+    l,r=map(int,input().split())
+    prefix[l]+=1
+    prefix[r+1]-=1
+for i in range(1,len(prefix)):
+    prefix[i]+=prefix[i-1]
+
+for i in range(len(prefix)):
+    if prefix[i]<k:
+        prefix[i]=0
+    else:
+        prefix[i]=1
+for i in range(1,len(prefix)):
+    prefix[i]+=prefix[i-1]
+for i in range(q):
+    l,r=map(int,input().split())
+    if l==0:
+        print(prefix[r])
+    else:
+        print(prefix[r]-prefix[l-1])
