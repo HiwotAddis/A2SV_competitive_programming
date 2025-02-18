4a53a02bf8c6dec12b60f672438b75ec5a380af1
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2025-02-18 15:28:21 +0300

    Created 'Books' by A2SV Hub

---
+++ 18-Feb-2025/Books 271690.py
@@ -0,0 +1,15 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,t=map(int,input().split())
+lst=list(map(int,input().split()))
+max_=0
+Sum=0
+l=0
+for i in range(n):
+    Sum+=lst[i]
+    while Sum>t:
+        Sum-=lst[l]
+        l+=1
+    max_=max(max_,i-l+1)
+print(max_)
+    
