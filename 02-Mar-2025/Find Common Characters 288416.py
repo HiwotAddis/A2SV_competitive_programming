b2a42703fcc39d7c968a966dd0fa0a995b68695b
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2025-03-02 23:13:06 +0300

    Created 'Find Common Characters' by A2SV Hub

---
+++ 02-Mar-2025/Find Common Characters 288416.py
@@ -0,0 +1,11 @@
+# Problem: Find Common Characters - https://leetcode.com/problems/find-common-characters/
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        char_count = Counter(words[0])
+        for word in words[1:]:
+            char_count &= Counter(word)
+        result = []
+        for char, count in char_count.items():
+            result.extend([char] * count)
+        return result
