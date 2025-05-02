5019644d870c29c10d4e8e1bed97738aba39773b
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2025-05-02 22:01:07 +0300

    Created 'Runner-up Score' by A2SV Hub

---
+++ 02-May-2025/Runner-up Score 347281.py
@@ -0,0 +1,9 @@
+# Problem: Runner-up Score - https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    arr=set(arr)
+    arr=list(arr)
+    print( arr[-2])
